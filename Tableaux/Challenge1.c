dfee575b7ceac84170e913a23cc2f073c5d6ecbe
User <user@example.com>
2025-09-03 10:09:45 +0100

    Initialisation et affichage d'un tableau en C

---
+++ Tableaux/Challenge1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Initialisation 
+    int tableau[5] = {1, 2, 3};
+
+    // Affichage 
+    printf("%d\n", tableau[0]);
+    printf("%d\n", tableau[1]);
+    printf("%d\n", tableau[2]);
+    printf("%d\n", tableau[3]);
+    printf("%d\n", tableau[4]);
+
+    return 0;
+}
+
+
+
+    
+
+ 
