58dc19514fc6a7ccee34b816b17acd6b65c48c73
User <user@example.com>
2025-09-03 10:47:48 +0100

    Affichage des elements pairs d'un tableau en C

---
+++ Tableaux/Challenge12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n ,i;
+
+    printf("Entrez le nombre d'elements : ");
+    scanf("%d", &n);
+
+    int tableau[n]; 
+
+
+    for ( i = 0; i < n; i++) {
+        printf("Entrez l'element %d : ", i + 1);
+        scanf("%d", &tableau[i]);
+    }
+
+
+    // Affichage des éléments pairs
+    printf("\nLes elements pairs sont :\n");
+    for (int i = 0; i < n; i++) {
+        if (tableau[i] % 2 == 0) {
+            printf("%d\n", tableau[i]);
+        }
+    }
+
+    return 0;
+}
